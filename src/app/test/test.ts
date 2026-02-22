97c31c33af73d62298ce617403f4c7410eb3e3fb
Kiran Anand <liteninkiran@hotmail.com>
2026-02-22 17:19:54 +0000

    02 - Updates on HttpClient

--- src/app/test/test.ts
+++ src/app/test/test.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { User } from '../user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test.html',
   styleUrl: './test.scss',
 })
-export class Test {
+export class Test implements OnInit {
+  private userService = inject(User);
+  public users: any;
 
+  ngOnInit() {
+    this.userData();
+  }
+
+  userData() {
+    this.userService.getData().subscribe((data) => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
-  
+  private baseUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  private http = inject(HttpClient);
+
+  getData() {
+    return this.http.get(this.baseUrl);
+  }
 }
